af47d64f693265b369a2aca9a186fd93cce8d8d3
FloStich <f.stich@hotmail.com>
2021-05-23 08:25:27 +0200

    Company Treeset and Product onStock

--- Product.java
+++ Product.java
@@ -1,10 +1,12 @@
 public class Product
 {
 	private String name;
+	private boolean onStock;
 	
-	public Product(String name)
+	public Product(String name, boolean onstock)
 	{
 		setName("Coca Cola");
+		setOnStock(true);
 	}
 	
 	public void setName(String name)
@@ -12,8 +14,18 @@ public class Product
 		this.name = name;
 	}
 	
+	public void setOnStock(boolean onStock)
+	{
+		this.onStock = onStock;
+	}
+	
 	public String getName()
 	{
 		return name;
+	}
+	
+	public boolean getOnStock()
+	{
+		return onStock;
 	}
 }
